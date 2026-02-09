225c4855cd269c7ab3728968f49371e34b663d41
Gustavo de Paula <Gustavodep02@users.noreply.github.com>
2026-02-09 17:44:29 -0300

    Add API Gateway and Eureka service discovery configurations

---
+++ eureka/src/test/java/com/gustavo/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
